9752ca6841c8f9c121ee1f659cede912e7cff42e
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2018-05-16 00:43:22 -0700

    embedded service and used URL connection for HTTP GET request

--- src/main/java/com/mkotsollaris/mec/service/ProductService.java
+++ src/main/java/com/mkotsollaris/mec/service/ProductService.java
@@ -1,17 +1,38 @@
 package com.mkotsollaris.mec.service;
 
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+@Service
 public class ProductService
 {
     public void getProductFromMecApiByKeywords(String... keywords)
             throws IOException
     {
-        URL url = new URL("http://example.com");
+        //e.g., http://www.mec.ca/api/v1/products/search?keywords=bike&doll&phone
+//        URL
+//                url =
+//                new URL("http://www.mec.ca/api/v1/products/search?keywords=" +
+//                                keywords);
+        URL url = new URL("http://www.mec.ca/api/v1/products/search?keywords=bike");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        System.out.println("content: "+content);
+        in.close();
+        con.disconnect();
     }
 
 }
